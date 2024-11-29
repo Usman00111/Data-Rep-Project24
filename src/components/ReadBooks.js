7c77d868ba2e39223dcc27afae0caae68cc7d844
Usman00111 <G00400065@atu.ie>
2024-11-29 18:32:29 +0000

    made a mistake in useEffect where i was calling iti back with id thats why it wasnt showing up when i was clicking on read books

--- src/components/ReadBooks.js
+++ src/components/ReadBooks.js
@@ -11,30 +11,30 @@ const ReadBooks = () => {
 
     //use effect hook fetches data from api
     useEffect(() => {
-        if (id) {
-            axios.get(`http://localhost:4000/api/books/${id}`) //get req made 
-            .then((response) => {
-                setBookData(response.data); // once it gets data from apu ir set teh data to the state and empty dependency array ensures data is fetched only once
-            })
-            .catch((error) => {
-                console.error("There was an error fetching the book data!", error);
-                setError("failed to load book data");
-            });
-        }
-    }, [id]); //runs when id changes
+        axios.get("http://localhost:4000/api/books")
+        .then((response) => {
+            setBookData(response.data); 
+        })
+        .catch((error) => {
+            console.error("There was an error fetching the book data!", error);
+            setError("Failed to load book data");
+        });
+}, []); // Dependency array is empty to only run once on mount
     
 
     return (
         <div>
-             {bookData ? (
-            <>
-                <h3>{bookData.title}</h3>
-                <p>Author: {bookData.author}</p>
-                <p>Year: {bookData.year}</p>
-                <img src={bookData.cover} alt={bookData.title} style={{ width: "200px" }} />
-            </>
+        {bookData ? (
+            bookData.map((book) => (
+                <div key={book._id}>
+                    <h3>{book.title}</h3>
+                    <p>Author: {book.author}</p>
+                    <p>Year: {book.year}</p>
+                    <img src={book.cover} alt={book.title} style={{ width: "200px" }} />
+                </div>
+            ))
         ) : (
-            <p>Loading book details...</p> // Add a loading state while data is being fetched
+            <p>Loading books...</p>
         )}
             {/*<h3>Hello from read component</h3>*/}
             {/*<Books books={bookData} /> {/*the books comp recieves bookdata as a prop and will render list of books based on this data */}
